8423c1d9f63bd45be38c9eb347d6078fe4264f14
Pavel Lovtsevich <p.lovtsevich@lovata.com>
2019-01-23 12:59:07 +0300

    New translations lang.php (Indonesian)

---
+++ lang/id/lang.php
@@ -0,0 +1,14 @@
+<?php return [
+    'plugin' => [
+        'name'        => 'Sphinx for Shopaholic',
+        'description' => ' Sphinx search by products, categories, brands, tags',
+    ],
+    'tab'    => [
+        'search_content'  => 'Search content',
+        'search_settings' => 'Search',
+    ],
+    'field'  => [
+        'search_synonym'       => 'Search synonyms',
+        'search_content'       => 'Search content',
+    ],
+];
